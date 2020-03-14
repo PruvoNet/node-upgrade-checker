7487cb50a0cceb674c29cca82726198c75a607b5
regevbr <regevbr@gmail.com>
2020-03-14 19:10:34 +0200

    fix coverage reports

--- nyc.config.js
+++ nyc.config.js
@@ -4,13 +4,12 @@ const {parserPlugins} = require('@istanbuljs/schema').defaults.nyc;
 
 module.exports = {
   cache: false,
-  parserPlugins: parserPlugins.concat(['typescript']),
-  sourceMap: false,
-  'produce-source-map': false,
   exclude: [
     'src/test/**',
     'src/testE2e/**',
-    'dist/**',
+    'dist/test/**',
+    'dist/testE2e/**',
+    'dist/**.ts',
     'nyc.config.js'
   ],
   all: true
