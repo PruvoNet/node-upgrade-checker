3334d60f18802b26f0ff938f7f8970ffb2cab767
regevbr <regevbr@gmail.com>
2020-03-17 00:23:35 +0200

    add lts tests

---
+++ test/lib/utils/lts/impl/lts.spec.ts
@@ -0,0 +1,64 @@
+'use strict';
+
+import {container} from '../../../../../src/container';
+import {ILts} from '../../../../../src/utils/lts';
+import moment = require('moment');
+
+const dateFormat = `YYYY-MM-DD`;
+
+describe('lts', () => {
+
+    let lts: ILts;
+
+    beforeEach(() => {
+        container.snapshot();
+        lts = container.get(ILts);
+    });
+
+    afterEach(() => {
+        container.restore();
+    });
+
+    it('should resolve to node 4', async () => {
+        const versions = await lts.resolveLtsVersion({
+            date: moment.utc('2015-10-02', dateFormat),
+        });
+        expect(versions).toEqual(['4']);
+    });
+
+    it('should resolve to node 4 & 6', async () => {
+        const versions = await lts.resolveLtsVersion({
+            date: moment.utc('2016-10-19', dateFormat),
+        });
+        expect(versions).toEqual(['4', '6']);
+    });
+
+    it('should resolve to node 4 & 6 & 8', async () => {
+        const versions = await lts.resolveLtsVersion({
+            date: moment.utc('2017-11-01', dateFormat),
+        });
+        expect(versions).toEqual(['4', '6', '8']);
+    });
+
+    it('should resolve to node 6 & 8 & 10', async () => {
+        const versions = await lts.resolveLtsVersion({
+            date: moment.utc('2018-11-01', dateFormat),
+        });
+        expect(versions).toEqual(['6', '8', '10']);
+    });
+
+    it('should resolve to node 8 & 10 && 12', async () => {
+        const versions = await lts.resolveLtsVersion({
+            date: moment.utc('2019-10-23', dateFormat),
+        });
+        expect(versions).toEqual(['8', '10', '12']);
+    });
+
+    it('should resolve to node 10 && 12 && 14', async () => {
+        const versions = await lts.resolveLtsVersion({
+            date: moment.utc('2020-10-21', dateFormat),
+        });
+        expect(versions).toEqual(['10', '12', '14']);
+    });
+
+});
