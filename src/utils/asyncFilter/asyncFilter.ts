6ceaa5df435e55a0f7ea3d3e57171823d1aff2a1
regevbr <regevbr@gmail.com>
2020-03-17 21:56:57 +0200

    add async filter utility

---
+++ src/utils/asyncFilter/asyncFilter.ts
@@ -0,0 +1,8 @@
+export const asyncFilter = async <T>(array: T[], callback: (value: T, index?: number) => Promise<boolean>)
+    : Promise<T[]> => {
+    const promises = array.map(async (value, index) => {
+        return await callback(value, index);
+    });
+    const results: boolean[] = await Promise.all(promises);
+    return array.filter((_, i) => results[i]);
+};
---
+++ test/src/utils/asyncFilter/asyncFilter.spec.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+import {asyncFilter} from '../../../../src/utils/asyncFilter/asyncFilter';
+
+const isEven = async (n: number): Promise<boolean> => {
+    return n % 2 === 0;
+};
+
+describe('async filter', () => {
+
+    it('should filter async', async () => {
+        const results = await asyncFilter([1, 2, 3, 4, 5], isEven);
+        expect(results).toEqual([2, 4]);
+    });
+
+});
