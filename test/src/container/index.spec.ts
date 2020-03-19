eb393ac970b17ded84a0c891ff4b14692027eda0
regevbr <regevbr@gmail.com>
2020-03-19 22:02:36 +0200

    feature: add unit tests for multi injects

---
+++ test/src/container/index.spec.ts
@@ -0,0 +1,38 @@
+import { container } from '../../../src/container';
+import { IEntity } from '../../../src/db/interfaces/entity';
+import { Dependency, DependencyVersion } from '../../../src/db';
+import { ISpecificCIResolver } from '../../../src/resolvers/ciResolver';
+import { TravisCiResolver } from '../../../src/resolvers/ciResolver/impl/resolvers/travis';
+import { GithubActionsResolver } from '../../../src/resolvers/ciResolver/impl/resolvers/github';
+import { CircleCiResolver } from '../../../src/resolvers/ciResolver/impl/resolvers/circle';
+import { AppVeyorResolver } from '../../../src/resolvers/ciResolver/impl/resolvers/appveyor';
+
+describe(`container`, () => {
+  beforeEach(() => {
+    container.snapshot();
+  });
+
+  afterEach(() => {
+    container.restore();
+  });
+
+  it(`Should inject all entities`, () => {
+    const entities = container.getAll(IEntity);
+    expect(entities.length).toBe(2);
+    expect(entities).toContain(Dependency);
+    expect(entities).toContain(DependencyVersion);
+  });
+
+  it(`Should inject all specific ci resolvers`, () => {
+    const entities = container.getAll(ISpecificCIResolver);
+    expect(entities).toHaveLength(4);
+    expect(entities).toEqual(
+      expect.arrayContaining([
+        expect.any(AppVeyorResolver),
+        expect.any(CircleCiResolver),
+        expect.any(GithubActionsResolver),
+        expect.any(TravisCiResolver),
+      ])
+    );
+  });
+});
