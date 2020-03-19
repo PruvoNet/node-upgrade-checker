8b2dd723ea19808c1b40b4e12dff54b89ca1dbfa
regevbr <regevbr@gmail.com>
2020-03-19 21:11:50 +0200

    feature: create tester helper for bindings

--- test/src/db/index.spec.ts
+++ test/src/db/index.spec.ts
@@ -10,42 +10,36 @@ import { DependencyRepositoryProvider } from '../../../src/db/impl/dependencyRep
 import { DependencyVersionRepositoryProvider } from '../../../src/db/impl/dependencyVersionRepositoryProvider';
 import { ConnectionProvider } from '../../../src/db/impl/connectionProvider';
 import { IEntity } from '../../../src/db/interfaces/entity';
+import { BindingTypes, testBindings } from '../../utils/bindingTester';
 
-describe(`db module container`, () => {
-  it(`should perform bindings properly`, async () => {
-    const inSingletonScopeMock = jest.fn();
-    const toSpy = {
-      inSingletonScope: inSingletonScopeMock,
-    };
-    const toMock = jest.fn();
-    toMock.mockReturnValue(toSpy);
-    const toConstantValueMock = jest.fn();
-    const bindSpy = {
-      to: toMock,
-      toConstantValue: toConstantValueMock,
-    };
-    const bindMock = jest.fn();
-    bindMock.mockReturnValue(bindSpy);
-    dbModulesBinder(bindMock);
-    expect(bindMock).toHaveBeenCalledTimes(5);
-    expect(bindMock).toHaveBeenNthCalledWith(1, IDependencyRepositoryProvider);
-    expect(bindMock).toHaveBeenNthCalledWith(2, IDependencyVersionRepositoryProvider);
-    expect(bindMock).toHaveBeenNthCalledWith(3, IConnectionProvider);
-    expect(bindMock).toHaveBeenNthCalledWith(4, IEntity);
-    expect(bindMock).toHaveBeenNthCalledWith(5, IEntity);
-
-    expect(toMock).toHaveBeenCalledTimes(3);
-    expect(toMock).toHaveBeenNthCalledWith(1, DependencyRepositoryProvider);
-    expect(toMock).toHaveBeenNthCalledWith(2, DependencyVersionRepositoryProvider);
-    expect(toMock).toHaveBeenNthCalledWith(3, ConnectionProvider);
-
-    expect(toConstantValueMock).toHaveBeenCalledTimes(2);
-    expect(toConstantValueMock).toHaveBeenNthCalledWith(1, Dependency);
-    expect(toConstantValueMock).toHaveBeenNthCalledWith(2, DependencyVersion);
-
-    expect(inSingletonScopeMock).toHaveBeenCalledTimes(3);
-    expect(inSingletonScopeMock).toHaveBeenNthCalledWith(1);
-    expect(inSingletonScopeMock).toHaveBeenNthCalledWith(2);
-    expect(inSingletonScopeMock).toHaveBeenNthCalledWith(3);
-  });
+testBindings({
+  name: `db module container`,
+  binderFn: dbModulesBinder,
+  bindings: [
+    {
+      binder: IDependencyRepositoryProvider,
+      binded: DependencyRepositoryProvider,
+      type: BindingTypes.SINGELTON,
+    },
+    {
+      binder: IDependencyVersionRepositoryProvider,
+      binded: DependencyVersionRepositoryProvider,
+      type: BindingTypes.SINGELTON,
+    },
+    {
+      binder: IConnectionProvider,
+      binded: ConnectionProvider,
+      type: BindingTypes.SINGELTON,
+    },
+    {
+      binder: IEntity,
+      binded: Dependency,
+      type: BindingTypes.CONSTANT,
+    },
+    {
+      binder: IEntity,
+      binded: DependencyVersion,
+      type: BindingTypes.CONSTANT,
+    },
+  ],
 });
---
+++ test/utils/bindingTester.ts
@@ -0,0 +1,75 @@
+import { interfaces } from 'inversify';
+import Bind = interfaces.Bind;
+
+// eslint-disable-next-line jest/no-export
+export type BinderFn = (bind: Bind) => void;
+
+// eslint-disable-next-line jest/no-export
+export enum BindingTypes {
+  SINGELTON = `SINGELTON`,
+  CONSTANT = `CONSTANT`,
+}
+
+// eslint-disable-next-line jest/no-export
+export interface IBindingToTest {
+  binder: any;
+  binded: any;
+  type: BindingTypes;
+}
+
+// eslint-disable-next-line jest/no-export
+export interface IBindingTestOptions {
+  bindings: IBindingToTest[];
+  binderFn: BinderFn;
+  name: string;
+}
+
+const getBindNmae = (bind: any): string => {
+  return bind.name || bind.constructor?.name || bind.toString?.() || bind;
+};
+
+// eslint-disable-next-line jest/no-export
+export const testBindings = ({ binderFn, bindings, name }: IBindingTestOptions): void => {
+  describe(name, () => {
+    const inSingletonScopeMock = jest.fn();
+    const toSpy = {
+      inSingletonScope: inSingletonScopeMock,
+    };
+    const toMock = jest.fn();
+    toMock.mockReturnValue(toSpy);
+    const toConstantValueMock = jest.fn();
+    const bindSpy = {
+      to: toMock,
+      toConstantValue: toConstantValueMock,
+    };
+    const bindMock = jest.fn();
+    bindMock.mockReturnValue(bindSpy);
+    binderFn(bindMock);
+    let bindExpectedCalls = 0;
+    let toExpectedCalls = 0;
+    let toConstantValueExpectedCalls = 0;
+    let inSingletonScopeExpectedCalls = 0;
+    bindings.forEach((binding) => {
+      const { binder, binded, type } = binding;
+      it(`Should bind ${getBindNmae(binder)} to ${getBindNmae(binded)} - ${type}`, () => {
+        bindExpectedCalls++;
+        expect(bindMock).toHaveBeenNthCalledWith(bindExpectedCalls, binder);
+        if (type === BindingTypes.SINGELTON) {
+          toExpectedCalls++;
+          inSingletonScopeExpectedCalls++;
+          expect(toMock).toHaveBeenNthCalledWith(toExpectedCalls, binded);
+          expect(inSingletonScopeMock).toHaveBeenNthCalledWith(inSingletonScopeExpectedCalls);
+        } else if (type === BindingTypes.CONSTANT) {
+          toConstantValueExpectedCalls++;
+          expect(toConstantValueMock).toHaveBeenNthCalledWith(toConstantValueExpectedCalls, binded);
+        }
+      });
+    });
+    it(`Should not call more bindings than expected`, () => {
+      expect(bindMock).toHaveBeenCalledTimes(bindExpectedCalls);
+      expect(toMock).toHaveBeenCalledTimes(toExpectedCalls);
+      expect(toConstantValueMock).toHaveBeenCalledTimes(toConstantValueExpectedCalls);
+      expect(inSingletonScopeMock).toHaveBeenCalledTimes(inSingletonScopeExpectedCalls);
+    });
+  });
+};
