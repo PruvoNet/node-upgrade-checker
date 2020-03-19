60f59742ee540baaaf005b0ef02a44f592da4e7f
regevbr <regevbr@gmail.com>
2020-03-19 23:37:59 +0200

    chore: add better appveoyr uint tests

--- src/resolvers/ciResolver/impl/resolvers/appveyor.ts
+++ src/resolvers/ciResolver/impl/resolvers/appveyor.ts
@@ -4,13 +4,20 @@ import { inject, injectable } from 'inversify';
 import { FS, TYPES } from '../../../../container/nodeModulesContainer';
 import { parse } from 'yaml';
 
-const nodeVersionRegex = /Install-Product node ([^\s]+)/i;
+const nodeVersionRegex = /node ([^\s]+)/i;
+const nodeVersionInstallRegex = /Install-Product node ([^\s]+)/i;
+const nvmInstallRegex = /nvm install ([^\s]+)/i;
 const nodeEnvRegex = /\$env:(.+)/i;
 
-const nodeVersionMapper = (command: string | undefined): string | undefined => {
+const nodeVersionInstallMapper = (command: string | undefined): string | undefined => {
   if (!command) {
     return;
   }
+  const match = nodeVersionInstallRegex.exec(command) || nvmInstallRegex.exec(command);
+  return match?.[1];
+};
+
+const nodeVersionMapper = (command: string): string | undefined => {
   const match = nodeVersionRegex.exec(command);
   return match?.[1];
 };
@@ -20,11 +27,25 @@ const emptyFilter = (command: any): boolean => {
 };
 
 const psObjectMapper = (command: any): string | undefined => {
-  return command.ps;
+  return command.ps || command.cmd || command.sh;
 };
 
 const envObjectMapper = (variable: string) => (command: any): string | undefined => {
   return command[variable];
+};
+
+const parseStack = (stack: string | undefined, versions: string[]) => {
+  if (!stack) {
+    return;
+  }
+  stack
+    .split(`,`)
+    .map(nodeVersionMapper)
+    .forEach((version) => {
+      if (version) {
+        versions.push(version);
+      }
+    });
 };
 
 const ciFileName = `.appveyor.yml`;
@@ -46,22 +67,32 @@ export class AppVeyorResolver extends ISpecificCIResolver {
     } catch (e) {
       return;
     }
+    const versions: string[] = [];
     const yaml = parse(fileContents);
+    const stack = yaml.stack;
+    parseStack(stack, versions);
     const installCommands: string[] = yaml.install;
+    if (!installCommands) {
+      return versions;
+    }
     const nodeVersion = installCommands
       .map(psObjectMapper)
-      .map(nodeVersionMapper)
+      .map(nodeVersionInstallMapper)
       .filter(emptyFilter)[0];
     if (!nodeVersion) {
-      throw new Error(`failed to located node version in install commands`);
+      return versions;
     }
     const match = nodeEnvRegex.exec(nodeVersion);
     if (!match) {
-      return [nodeVersion];
+      versions.push(nodeVersion);
+      return versions;
     } else {
       const matrixVarName = match[1];
-      const matrix: any[] = yaml.environment.matrix;
-      return matrix.map(envObjectMapper(matrixVarName)).filter(emptyFilter) as string[];
+      const environment: any[] = yaml.environment?.matrix || yaml.environment;
+      if (!environment) {
+        return versions;
+      }
+      return versions.concat(environment.map(envObjectMapper(matrixVarName)).filter(emptyFilter) as string[]);
     }
   }
 }
--- test/src/resolvers/ciResolver/impl/resolvers/appveyor.spec.ts
+++ test/src/resolvers/ciResolver/impl/resolvers/appveyor.spec.ts
@@ -19,28 +19,177 @@ describe(`appveyor`, () => {
     expect(appVeyorResolver.resolverName).toBe(`appVeyor`);
   });
 
-  it(`should resolve node js from travis configuration matrix`, async () => {
+  it(`should get file properly`, async () => {
+    fsMock.mockResolvedValue(`
+environment:
+  matrix:
+    - nodejs_version: "4"
+    - nodejs_version: "6.12"
+
+install:
+  - ps: Install-Product node $env:nodejs_version
+    `);
+    await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(fsMock).toBeCalledTimes(1);
+    expect(fsMock).toHaveBeenCalledWith(`placeholder/.appveyor.yml`, `utf-8`);
+  });
+
+  it(`should resolve node js from configuration matrix`, async () => {
+    fsMock.mockResolvedValue(`
+environment:
+  matrix:
+    - nodejs_version: "4"
+    - nodejs_version: "6.12"
+
+install:
+  - ps: Install-Product node $env:nodejs_version
+    `);
     const versions = await appVeyorResolver.resolve({
       repoPath,
     });
-    expect(versions).toEqual([`4`, `6`, `1.0`]);
+    expect(versions).toEqual([`4`, `6.12`]);
   });
 
-  it(`should resolve node js from travis configuration`, async () => {
+  it(`should return empty array if fails to find env element`, async () => {
+    fsMock.mockResolvedValue(`
+install:
+  - ps: Install-Product node $env:nodejs_version
+    `);
     const versions = await appVeyorResolver.resolve({
       repoPath,
     });
-    expect(versions).toEqual([`8`]);
+    expect(versions).toEqual([]);
   });
 
-  it(`should throw due to faulty configuration`, async () => {
-    const promise = appVeyorResolver.resolve({
+  it(`should return empty array if fails to find env variable`, async () => {
+    fsMock.mockResolvedValue(`
+environment:
+  matrix:
+    - foo: 4
+
+install:
+  - ps: Install-Product node $env:nodejs_version
+    `);
+    const versions = await appVeyorResolver.resolve({
       repoPath,
     });
-    await expect(promise).rejects.toBeInstanceOf(Error);
+    expect(versions).toEqual([]);
+  });
+
+  it(`should resolve node js from configuration environment`, async () => {
+    fsMock.mockResolvedValue(`
+environment:
+  - nodejs_version: "4"
+  - nodejs_version: "6.12"
+
+install:
+  - ps: Install-Product node $env:nodejs_version
+  - sh: dummy command
+    `);
+    const versions = await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(versions).toEqual([`4`, `6.12`]);
+  });
+
+  it(`should resolve node js from configuration environment with stack that has no node version in it`, async () => {
+    fsMock.mockResolvedValue(`
+stack: mysql
+environment:
+  - nodejs_version: "4"
+  - nodejs_version: "6.12"
+
+install:
+  - ps: Install-Product node $env:nodejs_version
+  - fds: dummy command
+    `);
+    const versions = await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(versions).toEqual([`4`, `6.12`]);
+  });
+
+  it(`should resolve node js from ps install command`, async () => {
+    fsMock.mockResolvedValue(`
+install:
+  - ps: Install-Product node 8.12 x64
+    `);
+    const versions = await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(versions).toEqual([`8.12`]);
+  });
+
+  it(`should resolve node js from cmd install command`, async () => {
+    fsMock.mockResolvedValue(`
+install:
+  - cmd: powershell Install-Product node 8.12 x64
+    `);
+    const versions = await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(versions).toEqual([`8.12`]);
+  });
+
+  it(`should resolve node js from nvm install command`, async () => {
+    fsMock.mockResolvedValue(`
+install:
+  - sh: nvm install 8.12
+    `);
+    const versions = await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(versions).toEqual([`8.12`]);
+  });
+
+  it(`should resolve node js from nvm install command and stack`, async () => {
+    fsMock.mockResolvedValue(`
+stack: node 9
+install:
+  - sh: nvm install 8.12
+    `);
+    const versions = await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(versions).toEqual([`9`, `8.12`]);
+  });
+
+  it(`should resolve node js from stack configuration`, async () => {
+    fsMock.mockResolvedValue(`
+stack: mysql, node 8.12
+    `);
+    const versions = await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(versions).toEqual([`8.12`]);
+  });
+
+  it(`should return empty array when failed to find install commands`, async () => {
+    fsMock.mockResolvedValue(`
+install:
+  - ps: Install-Product bash
+    `);
+    const versions = await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(versions).toEqual([]);
+  });
+
+  it(`should return empty array when no install element`, async () => {
+    fsMock.mockResolvedValue(`
+init:
+  - ps: Install-Product bash
+    `);
+    const versions = await appVeyorResolver.resolve({
+      repoPath,
+    });
+    expect(versions).toEqual([]);
   });
 
   it(`should return undefined from non relevant repo`, async () => {
+    fsMock.mockRejectedValue(new Error());
     const versions = await appVeyorResolver.resolve({
       repoPath,
     });
